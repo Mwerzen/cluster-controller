51e5a788913e0e4395f744b9bbe545c0d2dc6c7b
Mike <mwerzen@gmail.com>
2017-04-23 18:43:23 -0400

    up

--- deployment-scripts/deployment/deploy-application.py
+++ deployment-scripts/deployment/deploy-application.py
@@ -4,13 +4,13 @@ import sys
 
 def main():
     command = {}
-    command['applicationName'] = "test"
-    command['applicationVersion'] = "4.0"
-    command['applicationCommand'] = "/home/pi/apps/getAndExec.sh testDeployment.sh"
-    command['replicationFactor'] = 1
+    command['applicationName'] = "DummyService"
+    command['applicationVersion'] = "6.0"
+    command['applicationCommand'] = "/home/pi/apps/getAndExec.sh DummyService.sh"
+    command['replicationFactor'] = 3
     command['keepOldVersions'] = False;
 
-    resp = requests.post('http://192.168.1.50:8080/deploy', json=command)
+    resp = requests.post('http://192.168.1.50:8081/deploy', json=command)
     
     if resp.status_code == 200:
         print("Deployed Successfully")
